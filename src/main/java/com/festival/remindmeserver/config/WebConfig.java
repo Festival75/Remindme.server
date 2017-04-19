f625a19320e0425b0763f50ae6917c5e30c9318d
Mikhail Tolstoy <festivalmg75@gmail.com>
2017-04-19 16:13:09 +0500

    HelloWorld

---
+++ src/main/java/com/festival/remindmeserver/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.festival.remindmeserver.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.festival.remindmeserver")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+
+
+}
